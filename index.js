c5f3ebedacf4f3b067cd894a9e5b82325e0edf06
fahmiazis <fahmiazis797@gmail.com>
2025-07-13 08:35:15 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.get('/convert', async (req, res) => {
 
   try {
     console.log("Starting download for:", url);
+    console.log('king')
 
     const today = new Date();
     const dateStr = `${today.getFullYear()}${(today.getMonth()+1+"").padStart(2,"0")}${(today.getDate()+"").padStart(2,"0")}`;
